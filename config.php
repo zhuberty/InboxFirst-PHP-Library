185f4e13039e319895fa7ed619e80d2c419e47b8
Zachary Huberty <zachary.j.huberty@gmail.com>
2016-04-12 22:41:17 -0400

    Added ignore for config file

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 /* These are the main configuration settings for this PHP module */
 
-define("API_KEY", "");
-define("ORG_ID", 0);
+define("API_KEY", "ba1d16ed4b6b1e87044b558ca1d3f2364e4b77d8");
+define("ORG_ID", 475);
